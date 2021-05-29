f20113382c22f8dc2e3f8d79c0da5e95a11abbb9
Isaque F <isaquefrankli@gmail.com>
2021-05-28 21:25:51 -0300

    Third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,11 +27,18 @@ function Header(props) {
     }, [userName])
 
     const handleAuth = () => {
-        auth.signInWithPopup(provider).then((result) => {
-            setUser(result.user);
-        }).catch((error) => {
-            alert(error.message)
-        })
+        if(!userName) {
+            auth.signInWithPopup(provider).then((result) => {
+                setUser(result.user);
+            }).catch((error) => {
+                alert(error.message)
+            })
+        }else if(userName) {
+            auth.signOut().then(() => {
+                dispatch(setSignOutState());
+                history.push('/')
+            }).catch((err) => alert(err.message))
+        }
     }
 
     const setUser = (user) => {
@@ -77,7 +84,12 @@ function Header(props) {
                         <span>SERIES</span>
                     </a>
                 </NavMenu>
-                <UserImg src={userPhoto} alt={userName} />
+                <SignOut>
+                    <UserImg src={userPhoto} alt={userName} />
+                    <DropDown>
+                        <span onClick={handleAuth}>Sign Out</span>
+                    </DropDown>
+                </SignOut>
                 </>
             )}
             </Nav>
@@ -185,6 +197,7 @@ const Login = styled.a`
     border: 1px solid #f9f9f9;
     border-radius: 4px;
     transition: all 0.2s ease 0s;
+    cursor: pointer;
 
     &:hover {
         background-color: #f9f9f9;
@@ -196,6 +209,44 @@ const Login = styled.a`
 const UserImg = styled.img`
     height: 100%;
 
+`;
+
+const DropDown = styled.div`
+    position: absolute;
+    top: 48px;
+    right: 0px;
+    background: rgb(19, 19, 19);
+    border: 1px solid rgba(151, 151, 151, 0.34);
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+    padding: 18px;
+    font-size: 14px;
+    letter-spacing: 3px;
+    width: 100px;
+    opacity: 0;
+`;
+
+const SignOut = styled.div`
+    position: relative;
+    display: flex;
+    height: 48px;
+    width: 48px;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+
+    ${UserImg} {
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+    }
+
+    &:hover {
+        ${DropDown} {
+            opacity: 1;
+            transition-duration: 1s;
+        }
+    }
 `;
 
 export default Header
